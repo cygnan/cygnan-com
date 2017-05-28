17e54d62535c2211ad23332ef2571a7d79169050
Cygnan <mail@cygnan.com>
2017-05-29 03:38:58 +0900

    Removed unused fields from script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,14 @@
 $(document).ready(function () {
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered.
-    $('.modal').modal();
+    $(".modal").modal();
 })
 
 $(document).ready(function () {
     // 描画速度 [frame/s]
     const FPS = 5;
 
-    // 角速度 [rad/s]
-    const W = 2 * Math.PI * (0.5 / 180);
-
-    const CENTER_X = 30 / 2 - 18 / 2;
-    const CENTER_Y = 30 / 2 - 18 / 2;
-    const RADIUS = 30;
-
-    // 初期位相 [rad]
-    const PH = {
-        "posgithub": (2 * Math.PI / 5) * 0,
-        "postwitter": (2 * Math.PI / 5) * 1,
-        "poskeybase": (2 * Math.PI / 5) * 2,
-        "possoundcloud": (2 * Math.PI / 5) * 3,
-        "posemail": (2 * Math.PI / 5) * 4
-    }
+    // FPS（描画速度）から計算されるインターバル（周期） [ms]
+    const INTERVAL = 1000 / FPS;
 
     // map関数用の配列
     const E = [
@@ -32,15 +19,28 @@ $(document).ready(function () {
         "posemail"
     ];
 
-    // FPS（描画速度）から計算されるインターバル（周期） [ms]
-    const INTERVAL = 1000 / FPS
+    // 角速度 [rad/s]
+    const W = 2 * Math.PI * (2 / 180);
+
+    const CENTER_X = 30 / 2 - 18 / 2;
+    const CENTER_Y = 30 / 2 - 18 / 2;
+    const RADIUS = 30;
+
+    // 初期位相 [rad]
+    const PH = {
+        "posgithub": Math.floor((2 * Math.PI / 5) * 1000) / 1000 * 0,
+        "postwitter": Math.floor((2 * Math.PI / 5) * 1000) / 1000 * 1,
+        "poskeybase": Math.floor((2 * Math.PI / 5) * 1000) / 1000 * 2,
+        "possoundcloud": Math.floor((2 * Math.PI / 5) * 1000) / 1000 * 3,
+        "posemail": Math.floor((2 * Math.PI / 5) * 1000) / 1000 * 4
+    }
 
     let t = 0;
 
     setInterval(function () {
         E.map(function (e) {
-            let x = Math.cos(W * t + PH[e]) * RADIUS + CENTER_X;
-            let y = Math.sin(W * t + PH[e]) * RADIUS * (-1) + CENTER_Y;
+            let x = Math.floor(Math.cos(W * t + PH[e]) * 1000) / 1000 * RADIUS + CENTER_X;
+            let y = Math.floor(Math.sin(W * t + PH[e]) * 1000) / 1000 * RADIUS * (-1) + CENTER_Y;
 
             $('#' + e).css(
                 {
@@ -55,44 +55,3 @@ $(document).ready(function () {
         t += INTERVAL / 1000;
     }, INTERVAL)
 });
-
-/*
-var $point = $('#pos-github'),
-    startPosition = 0, // 開始位置（角度）
-    duration = 5000, // 1週の時間
-    radiusX = 30,
-    radiusY = 30,
-    angle = 0,
-    radian = 0,
-    xPosition = 0,
-    yPosition = 0
-
-var start = function () {
-    $point.animate(
-        {
-            count: 2
-        },
-        {
-            duration: duration,
-            easing: 'linear',
-            step: function (now) {
-                angle = now * 180;
-                radian = ((angle + startPosition + 90) / 180) * Math.PI; // +90することで0度の基点を3時の角度に設定
-                xPosition = radiusX + (radiusX * Math.sin(radian));
-                yPosition = radiusY + (radiusY * Math.cos(radian)) * -1; // -1を乗算して時計回りにする
-
-                $point.css({
-                    left: xPosition,
-                    top: yPosition
-                });
-            },
-            complete: function () {
-                start();
-                this.count = 0;
-            }
-        }
-    );
-};
-
-start();
-*/
